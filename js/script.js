07ff426d2f0cb8d3ba7e2e6fdeff83a0ada6e786
PRONOB BISWAS <biswaspronob@outlook.com>
2024-02-12 05:18:38 +0600

    update

--- js/script.js
+++ js/script.js
@@ -1,12 +1,64 @@
+window.addEventListener('scroll', function(){
+    let winScroll = document.body.scrollTop || this.document.documentElement.scrollTop;
+    let winHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    let scrolled = (winScroll / winHeight) * 100;
+
+    let progress = document.querySelector(".progress");
+    progress.style.width = scrolled + "%";
+});
+
+
 let todoistTogle = document.querySelector('.todoistTogle');
 let togleball = document.querySelector('.togleball');
 
 todoistTogle.addEventListener("click", function(){
-    togleball.classList.add('togleLeft');
-    todoistTogle.style.backgroundColor = "red"
+    togleball.classList.toggle('togleLeft');
+    todoistTogle.classList.toggle('red');
+    
 });
 
-console.log(window.innerHeight);
-console.log(window.innerWidth);
-console.log(screen.width);
-console.log(screen.height);
+
+let continueBtnBox = document.querySelector('.continueBtnBox');
+let continueBtn = document.querySelector('.continueBtn');
+continueBtnBox.addEventListener("click" , function(){
+    document.body.scrollTop = 550;
+    document.documentElement.scrollTop = 650;
+
+});
+
+// let circles = document.querySelectorAll('.circle');
+// console.log(circles);
+// for(let i = 0; i < circles.length; i++){
+//     circles[i].addEventListener("click", function(){
+//         circles[i].classList.add('checkedUp');
+//         circles[i].classList.toggle('red');
+//         // circles[i].style.backgroundColor = "red"
+//     });
+
+// };
+// let circlesI = document.querySelectorAll('.circle i');
+// for(let i = 0; i < circlesI.length; i++){
+//     circlesI[i].addEventListener("click", function(){
+//         circlesI[i].style.opacity = "1"
+//         // circlesI[i].style.color = "#e73600"
+//         circlesI[i].style.color = "#fff"
+//     })
+// };
+
+let listItem = document.querySelectorAll('.stepTwoContent li');
+
+console.log(listItem);
+
+for(let i = 0 ; i < listItem.length ; i++){
+
+    listItem[i].addEventListener("click" , function(){
+
+        let circles = document.querySelectorAll('.circle');
+        for(let i = 0; i < circles.length ; i++){
+            // circles[i].style.backgroundColor = "red"
+            circles[i].classList.add("red")
+        }
+        console.log(circles[i]);
+    });
+
+}
